ed7e5b0e7366ab80a8382efdc90c16ca0aae7eff
omhp <43230968+omhp@users.noreply.github.com>
2019-06-18 23:47:55 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -14,7 +14,7 @@ function auto_safeconvert(){var a_to_vd=window.location.hostname;if(protected_li
 {protected_links=a_to_vd;}
 var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElementsByTagName("a");a_to_va=a_to_ve.length;a_to_vf=a_to_fa();a_to_vg=a_to_vf.length;var a_to_vh=false;var j=0;var a_to_vi="";for(var i=0;i<a_to_va;i++)
 {a_to_vh=false;j=0;while(a_to_vh==false&&j<a_to_vg)
-{a_to_vi=a_to_ve[i].href;if(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("http"))
+{a_to_vi=a_to_ve[i].href;if(a_to_vi.match(a_to_vf[j])||!a_to_vi||!a_to_vi.match("https"))
 {a_to_vh=true;}
 j++;}
 if(a_to_vh==false)
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="hydraulic-telescopic-cranes.html"}
 if(x=="2"){xxx="Trade-Forex-Without-Forex-Automatic-Trading-Software.html"}
 if(x=="3"){xxx="learn-how-to-trade-forex-little-cost.html"}
-if(x=="4"){xxx="the-best-way-to-trade-forex.html"}a_to_ve[i].href="http://www.omapk.xyz/2017/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="the-best-way-to-trade-forex.html"}a_to_ve[i].href="https://www.omapk.xyz/2017/04/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
